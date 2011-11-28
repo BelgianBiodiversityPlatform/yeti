2659e7ff6640138cae26833b1ec8cb6a80a97a76
Julien Cigar <jcigar@ulb.ac.be>
2011-11-28 12:53:05 +0100

    Added JSON.parse and Tools.Serializer

--- yeti.js
+++ yeti.js
@@ -23,6 +23,20 @@
 
     Yeti.JSON = new Object();
 
+    /* Yeti.JSON.parse
+     * Parses a string as JSON and returns the parsed value.
+     */
+
+    Yeti.JSON.parse = function(data) {
+        return typeof JSON != 'undefined' ?
+        JSON.parse(data) :
+        (function(src) {
+            // See RFC 4627
+            var json = !(/[^,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]/.test(src.replace(/"(\\.|[^"\\])*"/g, '')));
+            return json ? eval('(' + json + ')') : null;
+        })(data)
+    }
+
 
     /***********************************************************************
         DOM
@@ -49,7 +63,7 @@
     }
 
     /* Yeti.DOM.getElementsByClassName
-     * Returns a set of elements which have all the given class names. 
+     * Returns a set of elements which have all the given class names.
      */
 
     Yeti.DOM.getElementsByClassName = function(name, src) {
@@ -71,5 +85,52 @@
             return class_elems;
         })(name, src);
     }
+
+
+    /***********************************************************************
+        Tools
+    ************************************************************************/
+
+    Yeti.Tools = new Object();
+
+    /* Yeti.Tools.Serializer
+     * Serialize an object.
+     */
+
+    Yeti.Tools.Serializer = function(obj) {
+        this.qs = [];
+
+        for (var key in obj) {
+            this._encodeValue(key, obj[key]);
+        }
+    }
+
+    Yeti.Tools.Serializer.prototype.toString = function() {
+        return this.qs.join('&');
+    }
+
+    Yeti.Tools.Serializer.prototype._encodeValue = function(key, value) {
+        if (value !== undefined) {
+            switch (value.constructor) {
+                case Array:
+                    this._encodeArray(key, value);
+                    break;
+                default:
+                    this._encodeString(key, value);
+                    break;
+            }
+        }
+    }
+
+    Yeti.Tools.Serializer.prototype._encodeArray = function(key, value) {
+        for (var i=0, _len=value.length ; i<_len ; i++) {
+            this._encodeValue(key, value[i]);
+        }
+    }
+
+    Yeti.Tools.Serializer.prototype._encodeString = function(key, value) {
+        this.qs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+    }
+
 
 })(window);
